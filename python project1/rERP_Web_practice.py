053568481452219e62ebd8f83d92a264cdf70ef5
SA-RA-PARK <godsara416@naver.com>
2024-12-17 15:22:29 +0900

    Update rERP_Web_practice.py

--- python project1/rERP_Web_practice.py
+++ python project1/rERP_Web_practice.py
@@ -6,22 +6,19 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import os
 import util as ut
+import pandas as pd
+
 
 config_info = ut.load_config()       
 current_path = ut.exedir('py')
 #print(config_info) 
 config_info["folder"] = {
     "current" : current_path,
-    "work" : os.path.join(current_path, "work\\"),
-    "result" : os.path.join(current_path, "result\\")
-}
-#print("================") 
-#print(config_info["login"]["NAME"])                      
-
+    "RnD" : os.path.join(current_path, "RnD_list\\")
+} 
 
 # 폴더 새로 만들기
-ut.create_folder(config_info["folder"]["work"])      #파일이 없으면 만들고, 있으면 pass다 / work 폴더 만듦
-ut.create_folder(config_info["folder"]["result"])    #파일이 없으면 만들고, 있으면 pass다  / result 폴더 만듦
+ut.create_folder(config_info["folder"]["RnD"])      #파일이 없으면 만들고, 있으면 pass다 / RnD_list 폴더 만듦
 
 
 # 브라우저 꺼짐 방지 옵션
@@ -30,7 +27,8 @@ chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
 chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
 
-download_path = config_info["folder"]["work"]   # 저장할 위치를 지정하는 거다 / 위치는 위에서 새로만든 work
+# 저장할 위치 지정
+download_path = config_info["folder"]["RnD"] 
 
 chrome_options.add_experimental_option("prefs", {
 "download.default_directory": download_path,  # 다운로드 폴더 지정
@@ -76,12 +74,12 @@ driver.switch_to.frame(iframes[1])
 # 6. [연구비수주현황조회]-[연구기간] 변경
 input_element = driver.find_element(By.CSS_SELECTOR,config_info["period"]["startdate_CSS"])       #시작날짜박스 찾기
 input_element.click()                                                                             #찾은 날짜 박스 클릭
-input_element.clear()                                                                             #디폴트 날짜 지우기(방법1)
+input_element.clear()                                                                             #디폴트 날짜 지우기 (방법1)
 input_element.send_keys(config_info["period"]["start_dt"])                                        #원하는 끝나는 일자 입력
 sleep(2)
 
 input_element_e = driver.find_element(By.CSS_SELECTOR,config_info["period"]["enddate_CSS"])       #끝나는 날짜박스 찾기
-input_element_e.send_keys(Keys.BACKSPACE * 10)                                                    #디폴트 날짜 지우기(백스페이스 10번)(방법2)
+input_element_e.send_keys(Keys.BACKSPACE * 10)                                                    #디폴트 날짜 지우기(백스페이스 10번) (방법2)
 input_element_e.send_keys(config_info["period"]["end_dt"] + Keys.ENTER)                           #원하는 끝나는 일자 입력
 
 
@@ -94,9 +92,13 @@ sleep(3)
 
 # 8. [연구비수주현황조회]-[과제분류] 체크박스 풀기
 checkbox1 = driver.find_element(By.CSS_SELECTOR, "#PRJ_CATE_CD_02")    #연구시설장비비통합과제 체크박스 풀기
-checkbox2 = driver.find_element(By.CSS_SELECTOR, "#PRJ_CATE_CD_04")    #인건비풀링과제 체크박스 풀기          
+checkbox2 = driver.find_element(By.CSS_SELECTOR, "#PRJ_CATE_CD_03")    #간접비과제 체크박스 풀기  
+checkbox3 = driver.find_element(By.CSS_SELECTOR, "#PRJ_CATE_CD_04")    #인건비풀링과제 체크박스 풀기            
+checkbox4 = driver.find_element(By.CSS_SELECTOR, "#PRJ_CATE_CD_05")    #교내과제 체크박스 풀기     
 if checkbox1.is_selected(): checkbox1.click()     # 체크풀기(체크박스1)
 if checkbox2.is_selected(): checkbox2.click()     # 체크풀기(체크박스2)
+if checkbox3.is_selected(): checkbox3.click()     # 체크풀기(체크박스3)
+if checkbox4.is_selected(): checkbox4.click()     # 체크풀기(체크박스4)
 #if not checkbox.is_selected(): checkbox.click()  #체크안된거면 체크하기
 
 
@@ -126,59 +128,3 @@ driver.switch_to.default_content()
 
 # 13. 창 닫기
 driver.close() 
-
-
-"""
-
-
-
-##################### 파일이름 바꾸기 ###############################################
-
-# 14. 다운로드파일 이름 변경(오늘일자로)--안먹힘 ㅠㅠ
-# ①다운로드된 파일 확인 및 이름 변경 
-downloaded_files = os.listdir(config_info["folder"]["work"])
-
-# ②파일이 다운로드되었는지 확인하고 파일명 변경 
-today_date = datetime.today().strftime('%Y-%m-%d')
-
-for file in downloaded_files:
-    if file.endswith(".xlsx"):  # 파일 확장자에 맞게 수정
-        old_file_path = os.path.join(config_info["folder"]["work"], file)
-        new_file_name = f"{today_date}.xlsx"  # 오늘 날짜로 이름 변경
-        new_file_path = os.path.join(config_info["folder"]["work"], new_file_name)
-        os.rename(old_file_path, new_file_path)
-        print(f"파일이 '{new_file_name}'로 저장되었습니다.") 
-        break 
-
-######################################################################################
-
-
-
-######################  iframe 찾기  ###################################################
-# 9. [연구비수주현황(연구자)] 들어가기
-# ①연구비수주현황(연구자) 클릭을 위한 iframe찾기
-iframes = driver.find_elements(By.TAG_NAME, 'iframe')           # 모든 iframe을 찾고, 올바른 iframe으로 전환
-for i, iframe in enumerate(iframes):
-    print(f"iframe {i}: {iframe.get_attribute('id')}, {iframe.get_attribute('id')}, {iframe.get_attribute('src')}")
-
-# ②찾은 iframe으로 이동하기 (연구비수주현황(연구자) iframe으로 이동)
-driver.switch_to.frame(iframes[1])    #/rstat_0001_01.act?MENU_SEQ=205 (이 번호는 검사해서 찾아보면 확인가능) → iframe 1번이랑 번호가 같음(205)
-
-# ③연구비수주현황(연구자) 클릭
-driver.find_element(By.CSS_SELECTOR, "#menu_id_680 > a").click()   
-
-# 10. 다시 원래 프레임으로 돌아오기
-driver.switch_to.default_content()
-
-# 11. 컨텐츠 iframe 찾기
-#①
-iframes = driver.find_elements(By.TAG_NAME, 'iframe')           # 모든 iframe을 찾고, 올바른 iframe으로 전환
-for i, iframe in enumerate(iframes):
-    print(f"iframe {i}: {iframe.get_attribute('id')}, {iframe.get_attribute('id')}, {iframe.get_attribute('src')}")
-
-# ②찾은 iframe으로 이동하기 (연구비수주현황(연구자) iframe으로 이동)
-driver.switch_to.frame(iframes[2])    #/rstat_0040_01.act?MENU_SEQ=680 (이 번호는 검사해서 찾아보면 확인가능) → iframe 2번이랑 번호가 같음(680)
-
-# ③연구비수주현황(연구자) 조회버튼 클릭
-driver.find_element(By.CSS_SELECTOR, "#btnSearch").click()
-"""
